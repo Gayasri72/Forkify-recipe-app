14ba84f91704c0c8d0cf484e47cb8e850db47486
Gayasri72 <gayasripethum1@gmail.com>
2025-08-20 05:23:25 +0530

    implement book mark fuction and add recipe function

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes';
 export const TIMEOUT_SEC = 60;
 export const RES_PER_PAGE = 10;
-// export const KEY = '<YOUR_KEY>';
-// export const MODAL_CLOSE_SEC = 2.5;
+export const KEY = '1e0d3967-3f9c-461c-8856-81faa3cb8ab3';
+export const MODAL_CLOSE_SEC = 2.5;
